82932ca0c5208aa061724fee1233544c0a5c8992
RuthvikSjanamatti <12345rsjanamatti@gmail.com>
2025-04-27 18:37:40 +0530

    implemented search feature

--- search_student.c
+++ search_student.c
@@ -1,6 +1,32 @@
 // search_student.c
+#include <stdio.h>
+#include <stdlib.h>
 #include "student.h"
 
 void search_student() {
-    // To be implemented by Member D
+    int roll, found = 0;
+    Student s;
+    FILE *fp = fopen("students.txt", "rb");
+
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return;
+    }
+
+    printf("Enter Roll Number to search: ");
+    scanf("%d", &roll);
+
+    while (fread(&s, sizeof(Student), 1, fp)) {
+        if (s.roll == roll) {
+            printf("\nStudent Found:\n");
+            printf("Roll: %d\nName: %s\nAge: %d\n", s.roll, s.name, s.age);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found)
+        printf("Student with Roll %d not found.\n", roll);
+
+    fclose(fp);
 }
