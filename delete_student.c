01018864308c7a089d923bdbb0490b3608907b1f
sahana <mahalingapursahana@gmail.com>
2025-04-27 18:11:32 +0530

    implemented delete feature

--- delete_student.c
+++ delete_student.c
@@ -1,6 +1,38 @@
 // delete_student.c
+#include <stdio.h>
+#include <stdlib.h>
 #include "student.h"
 
 void delete_student() {
-    // To be implemented by Member C
+    int roll, found = 0;
+    Student s;
+    FILE *fp = fopen("students.txt", "rb");
+    FILE *temp = fopen("temp.txt", "wb");
+
+    if (fp == NULL || temp == NULL) {
+        printf("Error opening file!\n");
+        return;
+    }
+
+    printf("Enter Roll Number to delete: ");
+    scanf("%d", &roll);
+
+    while (fread(&s, sizeof(Student), 1, fp)) {
+        if (s.roll == roll) {
+            found = 1;
+        } else {
+            fwrite(&s, sizeof(Student), 1, temp);
+        }
+    }
+
+    fclose(fp);
+    fclose(temp);
+
+    remove("students.txt");
+    rename("temp.txt", "students.txt");
+
+    if (found)
+        printf("Student deleted successfully!\n");
+    else
+        printf("Student not found!\n");
 }
